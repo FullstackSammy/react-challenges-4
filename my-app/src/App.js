6a58a92cca091afbc9706fd2c6bdd500195a5c5d
Samuel Näsström <nasstromsamuel@icloud.com>
2022-12-13 13:41:10 +0000

    Finished challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,12 @@ import css from "./App.module.css";
 // import NavBarSimple from "./components/NavBarSimple";
 import Sidebar from "./components/Sidebar";
 import NavBarForm from "./components/NavBarForm";
+import Content from "./components/Content";
 
 function App() {
   return (
     <div className={css.App}>
-      {/* Add your components here */}
+      <Content />
       <NavBarForm />
       <Sidebar />
     </div>
